625a8482e26b0005977c2a0c170806485b00ad37
arturazarov <artur.azarau@gmail.com>
2018-02-19 12:21:37 +0300

    Project structure refactored

---
+++ PhotoAssistant/Application Delegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PhotoAssistant
+//
+//  Created by Артур Азаров on 19.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PhotoAssistant/View Controllers/RootViewController/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  PhotoAssistant
+//
+//  Created by Артур Азаров on 19.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+final class RootController: UIViewController {
+}
+
