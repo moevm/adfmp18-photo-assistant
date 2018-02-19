05e7736daf10c54d900237accfda9a7d5afb939b
arturazarov <artur.azarau@gmail.com>
2018-02-19 12:05:32 +0300

    Initial Commit

---
+++ PhotoAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoAssistant
+//
+//  Created by Артур Азаров on 19.02.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
