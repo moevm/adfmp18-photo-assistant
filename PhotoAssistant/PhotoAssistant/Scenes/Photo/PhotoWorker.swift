1cb3df2310c30a4e0eb61241bad13c8e8e12af09
ArturAzarau <artur.azarau@gmail.com>
2018-04-23 09:16:41 +0300

    Revert "Final version"
    This reverts commit 446c015fd55008ff5b0ab56604468d2f08f91554.

---
+++ PhotoAssistant/PhotoAssistant/Scenes/Photo/PhotoWorker.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoWorker.swift
+//  PhotoAssistant
+//
+//  Created by Артур Азаров on 03.04.2018.
+//  Copyright (c) 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class PhotoWorker
+{
+  func doSomeWork()
+  {
+  }
+}
